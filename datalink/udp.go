7de49fb363c99c22647ae9f4b56d4bf3edac4636
wwhai <751957846@qq.com>
2024-05-21 23:42:01 +0800

    fix: fix ip string

--- datalink/udp.go
+++ datalink/udp.go
@@ -76,7 +76,7 @@ func dataLink(ipAddr string, port int) (DataLink, error) {
 		broadcast[i] = ipNet.IP[i] | ^ipNet.Mask[i]
 	}
 
-	udp, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("%s:%d", ipAddr, port))
+	udp, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("%s:%d", ip.String(), port))
 	conn, err := net.ListenUDP("udp", udp)
 	udpAddr := conn.LocalAddr().(*net.UDPAddr)
 	port = udpAddr.Port
